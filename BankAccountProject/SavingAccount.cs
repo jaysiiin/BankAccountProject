bc170129e92b8052e89dc023c064b568337f4c95
csirirathasuk14 <csirirathasuk14@gmail.com>
2016-09-23 12:53:19 -0400

    first commit

---
+++ BankAccountProject/SavingAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountProject
+{
+    class SavingAccount : Account
+    {
+        //fields
+
+        //properties
+
+        //constructors
+
+
+    }
+}
